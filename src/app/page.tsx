06c394c6d61b33f500f2ed4281d4c092da007061
AnujNegi007 <anujnegi708@gmail.com>
2025-07-14 20:00:24 +0530

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="bg-yellow-300">Hello Next js</h1>
+    <>
+      <h1 className="bg-yellow-300">Hello Next js</h1>
+      <Button>Click</Button>
+    </>
   );
 }
