f0e343a396acec4c426aed45877ede386c77e5b7
AnujNegi007 <anujnegi708@gmail.com>
2025-07-15 09:43:33 +0530

    add tabs component and integrate into home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <section className="bg-white">
-        <div className="container flex items-center justify-around py-24">
+        <div className="container flex items-center justify-around py-18">
           <div>
             <h1 className="text-6xl font-black font-sans">
               Super Delicious Pizza in <br />
@@ -26,6 +27,26 @@ export default function Home() {
               src={"/pizza-main.png"}
             />
           </div>
+        </div>
+      </section>
+      <section>
+        <div className="container px-24">
+          <Tabs defaultValue="pizza" className="w-[480px]">
+            <TabsList>
+              <TabsTrigger value="pizza" className="text-md">
+                Pizza
+              </TabsTrigger>
+              <TabsTrigger value="beverages" className="text-md">
+                Beverages
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="pizza">
+              <p>Pizza Content</p>
+            </TabsContent>
+            <TabsContent value="beverages">
+              <p>Beverages Content</p>
+            </TabsContent>
+          </Tabs>
         </div>
       </section>
     </>
