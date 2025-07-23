0e224f407619af21678e39a68a9c0fc8a9e46856
AnujNegi007 <anujnegi708@gmail.com>
2025-07-23 06:19:07 +0530

    set default value in tenant select box

--- src/components/custom/TenantSelect.tsx
+++ src/components/custom/TenantSelect.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import {
   Select,
   SelectContent,
@@ -19,15 +19,20 @@ function TenantSelect({ tenants }: { tenants: Tenant[] }) {
     router.push(`?tenantId=${value}`);
   };
 
+  const searchParams = useSearchParams();
+
   return (
-    <Select onValueChange={handleValueChange}>
+    <Select
+      onValueChange={handleValueChange}
+      defaultValue={searchParams.get("tenantId") as string}
+    >
       <SelectTrigger className="w-[180px] focus:ring-0 focus:border-0">
         <SelectValue placeholder="Select Restaurant" />
       </SelectTrigger>
       <SelectContent>
         <SelectGroup>
           {tenants.map((tenant: Tenant) => (
-            <SelectItem key={tenant.id} value={tenant.id}>
+            <SelectItem key={tenant.id} value={String(tenant.id)}>
               {tenant.name}
             </SelectItem>
           ))}
